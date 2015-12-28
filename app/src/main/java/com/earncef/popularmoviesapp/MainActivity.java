b0b131de2ea2347b00e3842d7a67719db44742db
Earncef Sequeira <earncef@earncef.com>
2015-12-28 15:43:48 +0530

    List view for movies with background loading of movie listing and Picasso for displaying images.

--- app/src/main/java/com/earncef/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/earncef/popularmoviesapp/MainActivity.java
@@ -1,15 +1,39 @@
 package com.earncef.popularmoviesapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+import android.content.Context;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout.LayoutParams;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private String url = "http://api.themoviedb.org/3/discover/movie?api_key=d54e4dec538ca7f4b9def82c0eae1d10&sort_by=";
+    private String thumbnailUrl = "http://image.tmdb.org/t/p/w185/";
+    private String sortBy = "popularity.desc";
+    private JSONArray movieData = new JSONArray();
+    public static String[] thumbnails = {};
+    protected ImageAdapter imageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +41,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        imageAdapter = new ImageAdapter(this);
+        GridView gridView = (GridView) findViewById(R.id.gridThumbnail);
+        gridView.setAdapter(imageAdapter);
     }
 
     @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        //Get the movie data
+        new LoadMoviesTask().execute(url + sortBy);
+    }
+
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -34,10 +71,119 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_sort_high_rating) {
+            sortBy = "vote_average.desc";
+            new LoadMoviesTask().execute(url + sortBy);
+            return true;
+        }
+
+        if (id == R.id.action_sort_popular) {
+            sortBy = "popularity.desc";
+            new LoadMoviesTask().execute(url + sortBy);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    protected void update() {
+        List<String> tThumbnails = new ArrayList<>();
+        for (int i = 0, size = movieData.length(); i < size; i++) {
+            try {
+                JSONObject movie = movieData.getJSONObject(i);
+                tThumbnails.add(thumbnailUrl + movie.getString("poster_path"));
+            } catch (Exception e) {
+                Log.d(TAG, "There was an error in accessing movie data.");
+            }
+        }
+        thumbnails = tThumbnails.toArray(new String[tThumbnails.size()]);
+
+        imageAdapter.notifyDataSetChanged();
+    }
+
+    private class LoadMoviesTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (Exception e) {
+                return "Unable to retrieve response.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject obj = new JSONObject(result);
+                movieData = obj.getJSONArray("results");
+                update();
+            } catch (Exception e) {
+                Log.d(TAG, "There was an error in parsing JSON response.");
+            }
+        }
+
+        protected String downloadUrl(String url) throws Exception {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Accept", "application/json");
+
+            int responseCode = con.getResponseCode();
+            Log.d(TAG, "Sending 'GET' request to URL : " + url);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+
+            String responseLine;
+            while ((responseLine = in.readLine()) != null) {
+                sb.append(responseLine).append("\n");
+            }
+            in.close();
+            con.disconnect();
+
+            return sb.toString();
+        }
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return thumbnails.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (null == convertView) {
+                imageView = new ImageView(mContext);
+                imageView.setMinimumWidth(60);
+                imageView.setMinimumHeight(80);
+                imageView.setAdjustViewBounds(true);
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            Picasso.with(mContext).load(thumbnails[position]).into(imageView);
+
+            return imageView;
+        }
+    }
+
+
 }
